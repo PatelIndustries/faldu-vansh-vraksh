fc426d27163c222a407e224f4779cc69353418a6
Devang Patel <devang.chat3@gmail.com>
2024-02-25 19:53:02 +0000

    Addign member component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MemberComponentComponent } from './member-component/member-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MemberComponentComponent
   ],
   imports: [
     BrowserModule,
